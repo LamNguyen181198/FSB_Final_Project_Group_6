c922599c00b15d360a9ef5da405f01e337ed0a0c
Lam Nguyen <ntlam1998@gmail.com>
2025-07-14 20:25:05 +0700

    Remove document_name and subject

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # main.py
 from fastapi import FastAPI, UploadFile, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List
 import sqlite3
 import os
 import shutil
@@ -26,8 +25,6 @@ def init_db():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS exam_documents (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            document_name TEXT NOT NULL,
-            subject TEXT NOT NULL,
             file_path TEXT NOT NULL
         )
     """)
@@ -40,15 +37,13 @@ init_db()
 def get_document():
     conn = sqlite3.connect("database/exam_documents.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT id, document_name, subject, file_path FROM exam_documents")
+    cursor.execute("SELECT id, file_path FROM exam_documents")
     rows = cursor.fetchall()
     conn.close()
-    return [{"id": row[0], "document_name": row[1], "subject": row[2], "file_path": row[3]} for row in rows]
+    return [{"id": row[0], "file_path": row[1]} for row in rows]
 
 @app.post("/add_document")
 def add_document(
-    document_name: str = Form(...),
-    subject: str = Form(...),
     file: UploadFile = Form(...)
 ):
     try:
@@ -59,17 +54,16 @@ def add_document(
         conn = sqlite3.connect("database/exam_documents.db")
         cursor = conn.cursor()
         cursor.execute("""
-            INSERT INTO exam_documents (document_name, subject, file_path)
-            VALUES (?, ?, ?)
-        """, (document_name, subject, file_location))
+            INSERT INTO exam_documents (file_path)
+            VALUES  (?)
+        """, (file_location))
         conn.commit()
         new_id = cursor.lastrowid
         conn.close()
 
         return {
             "id": new_id,
-            "document_name": document_name,
-            "subject": subject
+            "file_path": file_location
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
